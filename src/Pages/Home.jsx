31a01eb946f3dce814f6228ee605510908d0c52c
manoj-hubgit <manojannadurai2265@gmail.com>
2025-02-18 11:26:08 +0530

    changed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,7 +5,7 @@ import { Button } from "flowbite-react";
 const Home = () => {
   return (
     <div className="text-center mt-10">
-      <h1 className="text-4xl font-bold text-gray-900">Welcome to the Quiz App</h1>
+      <h1 className="text-4xl font-bold text-gray-900">Welcome to the Assessment App</h1>
       <p className="text-gray-700 mt-2">Test your knowledge with our quiz collections</p>
       <div className="mt-4 flex justify-center">
         <Link to="/user">
